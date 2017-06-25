cd63761d9df32a7c227712be657c69220aec5102
Aaron Parecki <aaron@parecki.com>
2017-06-25 15:50:38 -0700

    support json response for authorization code verification

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,12 +1,19 @@
 <?php
 use Firebase\JWT\JWT;
 
+function format_response($data, $content_type) {
+  if($content_type == 'application/json')
+    return json_encode($data);
+  else
+    return http_build_query($data);  
+}
+
 $app->get('/', function($format='html') use($app) {
   $res = $app->response();
 
-  $html = render('index', array(
+  $html = render('index', [
     'title' => 'IndieAuth Token Endpoint'
-  ));
+  ]);
   $res->body($html);
 });
 
@@ -14,7 +21,12 @@ $app->get('/', function($format='html') use($app) {
 $app->post('/token', function() use($app) {
 
   $req = $app->request();
-  $app->response()->headers()->set('Content-Type', 'application/x-www-form-urlencoded');
+
+  $content_type = 'application/x-www-form-urlencoded';
+  if($app->request()->headers()->get('Accept') == 'application/json') {
+    $content_type = 'application/json';
+  }
+  $app->response()->headers()->set('Content-Type', $content_type);
 
   $params = $req->params();
   
@@ -22,10 +34,10 @@ $app->post('/token', function() use($app) {
   // aaronparecki.com http://aaronparecki.com http://aaronparecki.com/
   // Normlize the value now (move this into a function in IndieAuth\Client later)
   if(!array_key_exists('me', $params) || !($me = IndieAuth\Client::normalizeMeURL($params['me']))) {
-    $app->response()->body(http_build_query(array(
+    $app->response()->body(format_response([
       'error' => 'invalid_parameter',
       'error_description' => 'The "me" parameter provided was not valid'
-    )));
+    ], $content_type));
     return;
   }
 
@@ -33,10 +45,10 @@ $app->post('/token', function() use($app) {
   $authorizationEndpoint = IndieAuth\Client::discoverAuthorizationEndpoint($me);
 
   if(!$authorizationEndpoint) {
-    $app->response()->body(http_build_query(array(
+    $app->response()->body(format_response([
       'error' => 'missing_authorization_endpoint',
       'error_description' => 'No authorization endpoint was discovered'
-    )));
+    ], $content_type));
     return;
   }
 
@@ -50,27 +62,27 @@ $app->post('/token', function() use($app) {
     // $auth['me']
     // $auth['scope']
 
-    $token_data = array(
+    $token_data = [
       'me' => $auth['me'],
       'issued_by' => 'https://' . Config::$hostname . '/token',
       'client_id' => $params['client_id'],
       'issued_at' => time(),
       'scope' => k($auth, 'scope', ''),
       'nonce' => mt_rand(1000000,pow(2,30))
-    );
+    ];
     $token = JWT::encode($token_data, Config::$jwtKey);
 
-    $app->response()->body(http_build_query(array(
+    $app->response()->body(format_response([
       'me' => $auth['me'],
       'scope' => $token_data['scope'],
       'access_token' => $token
-    )));
+    ], $content_type));
   } else {
-    $app->response()->body(http_build_query(array(
+    $app->response()->body(format_response([
       'error' => 'unknown_error',
       'error_description' => 'There was an unknown error verifying the authorization code.',
       'auth_response' => $auth
-    )));
+    ], $content_type));
   }
 });
 
@@ -80,9 +92,9 @@ $app->get('/token', function() use($app) {
 
   // Return a regular HTML page if no token was sent
   if($app->request()->headers()->get('Authorization') == '') {
-    $html = render('index', array(
+    $html = render('index', [
       'title' => 'IndieAuth Token Endpoint'
-    ));
+    ]);
     $app->response()->body($html);
     return;
   }
@@ -91,6 +103,7 @@ $app->get('/token', function() use($app) {
   if($app->request()->headers()->get('Accept') == 'application/json') {
     $content_type = 'application/json';
   }
+  $app->response()->headers()->set('Content-Type', $content_type);
 
   $tokenString = false;
   $error_description = false;
@@ -109,23 +122,16 @@ $app->get('/token', function() use($app) {
     }
 
     if($token) {
-      $app->response()->headers()->set('Content-Type', $content_type);
-      if($content_type == 'application/json')
-        $app->response()->body(json_encode($token));
-      else
-        $app->response()->body(http_build_query($token));
+      $app->response()->body(format_response($token, $content_type));
       return;
     }
   }
 
   $app->response()->setStatus(400);
-  $error = array(
+  $error = [
     'error' => 'unauthorized',
     'error_description' => ($error_description ?: 'An access token is required. Send an HTTP Authorization header such as \'Authorization: Bearer xxxxxx\'')
-  );
-  if($content_type == 'application/json')
-    $app->response()->body(json_encode($error));
-  else
-    $app->response()->body(http_build_query($error));
+  ];
+  $app->response()->body(format_response($error, $content_type));
 });
 
